176195b7829caa4a360a4d13f670d2ef1c4b2c15
Joshua Si <joshuasi777@gmail.com>
2021-02-03 03:08:28 -0800

    Clean starter code
    - Rename App to BruinChat
    - Remove unused App.js/App.css and move code to index.js
    - Ignore lint files

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+const App = () => {
+  return <div>BruinChat</div>;
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
