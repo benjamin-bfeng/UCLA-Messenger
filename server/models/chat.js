3f566135577ddf72e8e66c54f5a19c86da287cd3
Arnav Garg <arnavga@gmail.com>
2021-02-28 17:28:32 -0800

    split message/chat into 2 files

--- server/models/chat.js
+++ server/models/chat.js
@@ -1,23 +1,3 @@
-const { ObjectID, Timestamp } = require('mongodb');
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-
-
-const messageSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, required: true },
-    chat: { type: Schema.Types.ObjectId, required: true },
-    timestamp: { type: Timestamp, required: true }
-})
-
-messageSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    delete returnedObject.__v;
-    delete returnedObject.password;
-  },
-});
-
-messageSchema.plugin(uniqueValidator);
-
 const Message = mongoose.model('Message', messageSchema);
 module.exports = Message;
 
