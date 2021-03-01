10483e82266cc2ef92d688edb7860478b391d882
Arnav Garg <arnavga@gmail.com>
2021-02-28 17:37:30 -0800

    removed timestamp tracking

--- server/models/message.js
+++ server/models/message.js
@@ -5,13 +5,11 @@ const uniqueValidator = require('mongoose-unique-validator');
 const messageSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, required: true },
     chat: { type: mongoose.Schema.Types.ObjectId, required: true },
-    timestamp: { type: Timestamp, required: true }
 })
 
 messageSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     delete returnedObject.__v;
-    delete returnedObject.password;
   },
 });
 
