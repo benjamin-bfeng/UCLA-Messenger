fe9806d10666b1eb7d116036901c9d5a77076e5d
Arnav Garg <arnavga@gmail.com>
2021-02-28 17:03:48 -0800

    wrote chat models

---
+++ server/models/chat.js
@@ -0,0 +1,34 @@
+const { ObjectID, Timestamp } = require('mongodb');
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+
+const messageSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, required: true },
+    chat: { type: Schema.Types.ObjectId, required: true },
+    timestamp: { type: Timestamp, required: true }
+})
+
+messageSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    delete returnedObject.__v;
+    delete returnedObject.password;
+  },
+});
+
+messageSchema.plugin(uniqueValidator);
+
+const chatSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    users: mongoose.Schema.Types.ObjectId,
+    messages: String
+})
+
+chatSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    delete returnedObject.__v;
+    delete returnedObject.password;
+  },
+});
+
+chatSchema.plugin(uniqueValidator);
